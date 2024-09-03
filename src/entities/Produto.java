12e6420bb7eac04c5f6b5078dee902fe4801215b
Vinicius Mendes Dayrell <vinidayrell1@hotmail.com>
2024-09-03 13:43:07 -0300

    Implementação da classe produto

--- src/entities/Produto.java
+++ src/entities/Produto.java
@@ -1,5 +1,43 @@
 package entities;
 
 public class Produto {
+    private String nome;
+    private double preco;
+    private int codigo;
+    private int quantidadeEstoque;
+
+    public Produto(String nome, double preco, int codigo, int quantidadeEstoque) {
+        this.nome = nome;
+        this.preco = preco;
+        this.codigo = codigo;
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void adicionarEstoque(int quantidade) {
+        if (quantidade > 0) {
+            this.quantidadeEstoque += quantidade;
+        }
+    }
+
+    public boolean removerEstoque(int quantidade) {
+        if (quantidade > 0 && this.quantidadeEstoque >= quantidade) {
+            this.quantidadeEstoque -= quantidade;
+            return true;
+        }
+        return false;
+
+    }
 
 }
