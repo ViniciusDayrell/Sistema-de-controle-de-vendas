98b5367b552d06412d5e03aeaa46b10cb4abf612
Vinicius Mendes Dayrell <vinidayrell1@hotmail.com>
2024-09-03 14:08:06 -0300

    Implementação da classe Cliente

--- src/entities/Cliente.java
+++ src/entities/Cliente.java
@@ -1,5 +1,20 @@
 package entities;
 
 public class Cliente {
+    private String nome;
+    private int id;
+
+    public Cliente(String nome, int id) {
+        this.nome = nome;
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getId() {
+        return id;
+    }
 
 }
--- src/entities/Loja.java
+++ src/entities/Loja.java
@@ -1,4 +1,9 @@
 package entities;
+/*
+ * Vai gerenciar o catalogo, clientes e historico de vendas
+ * Metodos: cadastrarCliente(), adicionarProduto(), realizarVendas(),
+ * calcularTotalVenda(), historicoVendas()
+ */
 
 public class Loja {
 
