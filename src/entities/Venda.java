28b6080c63e9f3743e876bb28b3759965399b220
Vinicius Mendes Dayrell <vinidayrell1@hotmail.com>
2024-09-03 14:25:34 -0300

    Implementação da classe Venda

--- src/entities/Venda.java
+++ src/entities/Venda.java
@@ -1,5 +1,32 @@
 package entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Venda {
+    private List<Produto> produtos;
+    private Cliente cliente;
+    private int quantidade;
+
+    public Venda() {
+        this.produtos = new ArrayList<>();
+    }
+
+    public Venda(Cliente cliente, int quantidade) {
+        this.cliente = cliente;
+        this.quantidade = quantidade;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
 
 }
